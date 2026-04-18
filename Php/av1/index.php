a2bafef9899565e1024ad542ab0257a8861edd56
Mariana Machado <mariana.25104708360006@faeterj-rio.edu.br>
2026-04-18 08:58:55 -0300

    Finalizando arquivos av1

---
+++ Php/av1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Jogo COrporativo</title>
+</head>
+<body>
+	<h1>Bem vindo</h1><br><br>
+	<h2>Clique em uma das opções abaixo para começar</h2><br><br>
+	<form action="criarPergRespME.php">
+		<button>Criar novo cadastro</button><br><br>
+	</form>
+	<button>Criar nova Pergunta e resposta multipla escolha</button><<br><br>
+	<button>Criar nova Pergunta discursiva</button><<br><br>
+</body>
+</html>
